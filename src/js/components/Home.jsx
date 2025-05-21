eb6aa44818e51887066aacaca2d5546db748ff8d
Shane B <dotfortun@users.noreply.github.com>
2025-05-21 23:04:59 +0000

    Made the water clock prettier.

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,33 +1,74 @@
+import { useEffect, useState } from "react";
 import Container, { Col, Row } from "./Grid";
 import WaterClock from "./WaterClock";
 
-const Home = ({ time }) => {
+const Home = ({}) => {
+  const [time, setTime] = useState(new Date());
+  // const [value, setValue] = useState("Some state.");
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(value);
+  // }, [value]);
+
   return (
     <Container>
+      {/* <Row>
+        <Col>
+          <input
+            type="text"
+            value={value}
+            onChange={(ev) => setValue(ev.target.value)}
+          />
+        </Col>
+      </Row> */}
       <Row>
         {/* Hours 10s place */}
         <Col offset={{ xl: 3 }} width={{ xl: 1 }}>
-          <WaterClock fill={Math.floor(time.getHours() / 10) * 10} />
+          <WaterClock
+            fill={Math.floor(time.getHours() / 10) * 10}
+            fillColor={`hsl(${0} 95% 50%)`}
+          />
         </Col>
         {/* Hours 1s place */}
         <Col width={{ xl: 1 }}>
-          <WaterClock fill={(time.getHours() % 10) * 10} />
+          <WaterClock
+            fill={(time.getHours() % 10) * 10}
+            fillColor={`hsl(${(360 / 6) * 1} 95% 50%)`}
+          />
         </Col>
         {/* Minutes 10s place */}
         <Col width={{ xl: 1 }}>
-          <WaterClock fill={(Math.floor(time.getMinutes()) / 10) * 10} />
+          <WaterClock
+            fill={Math.floor(time.getMinutes() / 10) * 10}
+            fillColor={`hsl(${(360 / 6) * 2} 95% 50%)`}
+          />
         </Col>
         {/* Minutes 1s place */}
         <Col width={{ xl: 1 }}>
-          <WaterClock fill={(time.getMinutes() % 10) * 10} />
+          <WaterClock
+            fill={(time.getMinutes() % 10) * 10}
+            fillColor={`hsl(${(360 / 6) * 3} 95% 50%)`}
+          />
         </Col>
         {/* Seconds 10s place */}
         <Col width={{ xl: 1 }}>
-          <WaterClock fill={(Math.floor(time.getSeconds()) / 10) * 10} />
+          <WaterClock
+            fill={Math.floor(time.getSeconds() / 10) * 10}
+            fillColor={`hsl(${(360 / 6) * 4} 95% 50%)`}
+          />
         </Col>
         {/* Seconds 1s place */}
         <Col width={{ xl: 1 }}>
-          <WaterClock fill={(time.getSeconds() % 10) * 10} />
+          <WaterClock
+            fill={(time.getSeconds() % 10) * 10}
+            fillColor={`hsl(${(360 / 6) * 5} 95% 50%)`}
+          />
         </Col>
       </Row>
     </Container>
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -13,20 +13,8 @@ import Home from "./components/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-let timeInSeconds = 0;
-
-setInterval(() => {
-  timeInSeconds++;
-
-  root.render(
-    <React.StrictMode>
-      <div>{timeInSeconds.toString().padStart(6, "0")[0]}</div>
-      <div>{timeInSeconds.toString().padStart(6, "0")[1]}</div>
-      <div>{timeInSeconds.toString().padStart(6, "0")[2]}</div>
-      <div>{timeInSeconds.toString().padStart(6, "0")[3]}</div>
-      <div>{timeInSeconds.toString().padStart(6, "0")[4]}</div>
-      <div>{timeInSeconds.toString().padStart(6, "0")[5]}</div>
-      <Home time={new Date()} />
-    </React.StrictMode>
-  );
-}, 1000);
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
